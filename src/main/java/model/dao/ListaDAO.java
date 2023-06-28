96c4f03a1f21e8ef6d6537de1d901969c742c21c
marcus <marcus@DESKTOP-7NR0OC6>
2023-06-28 19:44:16 -0300

    metade da lista foi codada

--- src/main/java/model/dao/ListaDAO.java
+++ src/main/java/model/dao/ListaDAO.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.exception.ErroAtualizarException;
 import model.exception.ErroCadastroException;
 import model.exception.ErroConsultarException;
 import model.util.FormatadorData;
 import model.vo.Lista;
+import model.vo.ProdutosLista;
 
 public class ListaDAO {
-
-	private ArrayList<Lista> listas;
 
 	public boolean cadastrarLista(Lista lista) throws ErroCadastroException {
 		boolean cadastrado = false;
@@ -26,7 +26,7 @@ public class ListaDAO {
 		PreparedStatement pstmtListaProduto = Banco.getPreparedStatement(connection, insertListaProdutos);
 		try {
 			pstmtLista.setInt(1, lista.getIdCliente());
-			pstmtLista.setString(2, lista.getNomeLista());
+			pstmtLista.setString(2, lista.getNomeLista().toUpperCase());
 			pstmtLista.setObject(3, lista.getDataLista());
 			pstmtLista.execute();
 			ResultSet resultado = pstmtLista.getGeneratedKeys();
@@ -38,10 +38,12 @@ public class ListaDAO {
 					pstmtListaProduto.setObject(4, produto.getUnidadeMedida());
 					pstmtListaProduto.setDouble(4, produto.getValorMedida());
 					pstmtListaProduto.setString(6, produto.getObs());
+					
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			});
+			lista.setIdLista(resultado.getInt(1));
 			cadastrado = true;
 		} catch (SQLException e) {
 			throw new ErroCadastroException("Erro no metodo cadastrarLista, Erro ao cadastrar a lista");
@@ -54,7 +56,7 @@ public class ListaDAO {
 	}
 	
 	public List<Lista> consultarListasDAO(int idCliente) throws ErroConsultarException {
-		listas = new ArrayList<Lista>();
+		List<Lista> listas = new ArrayList<Lista>();
 		Connection connection = Banco.getConnection();
 		String sql = "select id_lista, nome, data_lista from lista where id_cliente = ?";
 		PreparedStatement pstmt = Banco.getPreparedStatement(connection, sql);
@@ -75,5 +77,45 @@ public class ListaDAO {
 			Banco.closeConnection(connection);
 		}
 		return listas;
+	}
+	
+	public Lista consultarPorId(int idLista) throws ErroConsultarException {
+		Lista lista = new Lista();
+		Connection connection = Banco.getConnection();
+		String sqlLista = "SELECT ID_LISTA, ID_CLIENTE, NOME, DATA_LISTA FROM LISTA WHERE ID_LISTA = ?";
+		PreparedStatement pstmt = Banco.getPreparedStatement(connection, sqlLista);
+		try {
+			ResultSet resultado = pstmt.executeQuery();
+			if(resultado.next()) {
+				lista.setIdLista(resultado.getInt(1));
+				lista.setIdCliente(resultado.getInt(2));
+				lista.setNomeLista(resultado.getString(3));
+				lista.setDataLista(FormatadorData.formatarDataMySQL(resultado.getString(5)));
+			}
+		} catch (SQLException e) {
+			throw new ErroConsultarException("Erro no método, consultarPorId, Erro ao consultar as listas");
+		} finally {
+			Banco.closePreparedStatement(pstmt);
+			Banco.closeConnection(connection);
+		}
+		return lista;
+	}
+	
+	public boolean atualizarLista(Lista lista) throws ErroAtualizarException {
+		boolean resultado = false;
+		Connection connection = Banco.getConnection();
+		String sql = "update lista set nome = ? where id_lista = ?";
+		PreparedStatement pstmt = Banco.getPreparedStatement(connection, sql);
+		
+		try {
+			pstmt.setString(1, lista.getNomeLista());
+			pstmt.setInt(2, lista.getIdLista());
+			if(pstmt.executeUpdate() == 1) {
+				resultado = true;
+			}
+		} catch (SQLException e) {
+			throw new ErroAtualizarException("Erro no método atualizarLista, Erro ao atualizar a lista");
+		}
+		return resultado;
 	}
 }
